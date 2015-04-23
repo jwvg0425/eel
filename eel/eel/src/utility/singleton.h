4ec07ca0695a93d2c311edebd0caa6f58095f0a3
jwvg0425 <jwvg0425@naver.com>
2015-04-23 17:28:08 +0900

    add singleton template class

---
+++ eel/eel/src/utility/singleton.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "base/macro.h"
+
+NS_EEL_BEGIN
+
+template<typename T>
+class Singleton
+{
+public:
+	static T* GetInstance()
+	{
+		if (m_Instance == nullptr)
+		{
+			m_Instance = new T;
+		}
+
+		return m_Instance;
+	}
+	
+	static void ReleaseInstance()
+	{
+		SAFE_DELETE(m_Instance);
+	}
+
+private:
+	static T* m_Instance;
+};
+
+template<typename T>
+T* Singleton<T>::m_Instance = nullptr;
+
+NS_EEL_END
