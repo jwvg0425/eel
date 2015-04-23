fc7e36b7614f447a616c6434341a3c3966d19469
jwvg0425 <jwvg0425@naver.com>
2015-04-23 20:28:23 +0900

    [template] add template project

---
+++ eel/template/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <Windows.h>
+#include "eel.h"
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
+	, LPSTR lpszCmdParam, int nCmdShow)
+{
+	eel::Application program;
+
+	program.Init(L"dummy client", 800, 600);
+
+	return program.Run();
+}
