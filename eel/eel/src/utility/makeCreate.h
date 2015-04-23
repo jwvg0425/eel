43378ece00ce713776c48c34a64976713a170617
jwvg0425 <jwvg0425@naver.com>
2015-04-24 01:27:46 +0900

    [engine] add object

---
+++ eel/eel/src/utility/makeCreate.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <memory>
+#include "base/macro.h"
+
+
+NS_EEL_BEGIN
+
+template<typename T>
+class MakeCreate
+{
+public:
+	template<typename... Args>
+	static std::shared_ptr<T> Create(Args&&... args)
+	{
+		auto ptr = std::make_shared<T>(std::forward<Args>(args)...);
+
+		return ptr;
+	}
+};
+
+NS_EEL_END
