5777e655c80f3224668ecb5b3b8fed7b165bf4cd
jwvg0425 <jwvg0425@naver.com>
2015-04-27 23:40:58 +0900

    [engine] add compare(stl compare function)

---
+++ src/utility/compare.h
@@ -0,0 +1,41 @@
+﻿#include "base/macro.h"
+
+NS_EEL_BEGIN
+
+template<typename T>
+bool Equal(T a, T b)
+{
+	return a == b;
+}
+
+template<typename T>
+bool NotEqual(T a, T b)
+{
+	return a != b;
+}
+
+template<typename T>
+bool More(T a, T b)
+{
+	return a > b;
+}
+
+template<typename T>
+bool Less(T a, T b)
+{
+	return a < b;
+}
+
+template<typename T>
+bool MoreEqual(T a, T b)
+{
+	return a >= b;
+}
+
+template<typename T>
+bool LessEqual(T a, T b)
+{
+	return a <= b;
+}
+
+NS_EEL_END
