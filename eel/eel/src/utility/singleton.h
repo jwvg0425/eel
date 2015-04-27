30e7336b1b15cbb8f7e88b439fd34ae91a36329e
jwvg0425 <jwvg0425@naver.com>
2015-04-27 20:22:15 +0900

    [engine] change singleton implementation(using static variable in GetInstance function)

--- eel/eel/src/utility/singleton.h
+++ eel/eel/src/utility/singleton.h
@@ -9,30 +9,18 @@ class Singleton
 public:
 	static T* GetInstance()
 	{
-		if (m_Instance == nullptr)
-		{
-			m_Instance = new T;
-		}
+		static T instance;
 
-		return m_Instance;
+		return &instance;
 	}
-	
-	static void ReleaseInstance()
-	{
-		SAFE_DELETE(m_Instance);
-	}
-
-	Singleton() = default;
-	~Singleton() = default;
 
 	Singleton(Singleton& rhs) = delete;
 	Singleton& operator=(Singleton& rhs) = delete;
 
-private:
-	static T* m_Instance;
-};
+protected:
+	Singleton() {}
+	~Singleton() {}
 
-template<typename T>
-T* Singleton<T>::m_Instance = nullptr;
+};
 
 NS_EEL_END
