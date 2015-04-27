1872e49ad3bbaa83b63042b93b9d1074c38ea1b7
jwvg0425 <jwvg0425@naver.com>
2015-04-27 20:21:09 +0900

    [template] test renderer

--- eel/template/main.cpp
+++ eel/template/main.cpp
@@ -8,5 +8,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
 
 	program.Init(L"dummy client", 800, 600);
 
+	eel::Renderer::GetInstance()->
+		SetBackgroundColor(eel::Color(0.6f, 0.3f, 0.3f, 1.0f));
+
 	return program.Run();
 }
