919e9bdc9b30d5d4c63e75cc72ac705bd52247e0
jwvg0425 <jwvg0425@naver.com>
2015-04-29 17:25:07 +0900

    [engine] texture(ID3D11Texture2D proxy class) added.

---
+++ src/render/texture.h
@@ -0,0 +1,19 @@
+﻿#include "d3dx11Effect.h"
+#include "base/object.h"
+#include "utility/makeCreate.h"
+
+NS_EEL_BEGIN
+
+class Texture : public Object, public MakeCreate<Texture>
+{
+public:
+	Texture(float width, float height);
+	~Texture() override;
+
+	operator ID3D11Texture2D*();
+
+private:
+	ID3D11Texture2D* m_Texture = nullptr;
+};
+
+NS_EEL_END
