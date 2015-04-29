f29e5bbbff7911996e989fc8aec3504a0ea5dc14
jwvg0425 <jwvg0425@naver.com>
2015-04-29 10:04:12 +0900

    [engine] render states, scene added

---
+++ src/component/scene.h
@@ -0,0 +1,13 @@
+﻿#include "component/component.h"
+
+NS_EEL_BEGIN
+class Scene : public Component
+{
+public:
+	Scene() = default;
+	~Scene() override = default;
+
+private:
+};
+
+NS_EEL_END
---
+++ src/render/renderStates.h
@@ -0,0 +1,9 @@
+﻿#include "base/macro.h"
+
+NS_EEL_BEGIN
+
+class RenderState
+{
+};
+
+NS_EEL_END
