fd05aca299adf2e5d38c9bb81262fd7ab94cb492
jwvg0425 <jwvg0425@naver.com>
2015-04-29 17:30:49 +0900

    [engine] render target added(not implemented yet).

---
+++ src/render/renderTarget.cpp
@@ -0,0 +1,1 @@
+﻿#include "renderTarget.h"
---
+++ src/render/renderTarget.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "d3dx11Effect.h"
+#include "base/object.h"
+#include "render/texture.h"
+
+NS_EEL_BEGIN
+
+class RenderTarget : public Object
+{
+public:
+	RenderTarget();
+	~RenderTarget() override;
+
+	RenderTarget(RenderTarget& rhs) = delete;
+	RenderTarget& operator=(RenderTarget& rhs) = delete;
+
+private:
+	UPTR<Texture>			m_DepthStencilBuffer = nullptr;
+	ID3D11RenderTargetView* m_RenderTargetView = nullptr;
+	ID3D11DepthStencilView* m_DepthStencilView = nullptr;
+	D3D11_VIEWPORT m_Viewport;
+};
+
+NS_EEL_END
