3c1c90f105fd193c48773758a5441f7e35298117
jwvg0425 <jwvg0425@naver.com>
2015-05-11 21:34:57 +0900

    [engine] camera method added(not impl)

--- src/render/camera/camera.cpp
+++ src/render/camera/camera.cpp
@@ -1,3 +1,44 @@
 ﻿#include "camera.h"
 
 USING_NS_EEL;
+
+eel::Camera::Camera(Point3 eyePos, Point3 targetPos, Vector3 up)
+	:m_EyePos(eyePos), m_Up(up)
+{
+
+}
+
+eel::Matrix4 eel::Camera::GetViewProjection() const
+{
+
+}
+
+void eel::Camera::Walk(float d)
+{
+
+}
+
+void eel::Camera::Strafe(float d)
+{
+
+}
+
+void eel::Camera::Pitch(float angle)
+{
+
+}
+
+void eel::Camera::RotateY(float angle)
+{
+
+}
+
+void eel::Camera::SetEyePos(Point3 eyePos)
+{
+
+}
+
+void eel::Camera::SetUpVector(Vector3 up)
+{
+
+}
