c468d62a644cff760c2b352941fc8dcf44b964d0
jwvg0425 <jwvg0425@naver.com>
2015-05-12 02:13:55 +0900

    [engine] perspective camera default lens added

--- src/render/camera/perspectiveCamera.cpp
+++ src/render/camera/perspectiveCamera.cpp
@@ -1,4 +1,5 @@
 ﻿#include "perspectiveCamera.h"
+#include "base/application.h"
 
 USING_NS_EEL;
 
@@ -10,5 +11,5 @@ void eel::PerspectiveCamera::SetLens(FLOAT fovAngle, FLOAT aspect, FLOAT nearZ, 
 eel::PerspectiveCamera::PerspectiveCamera(Point3 eyePos, Point3 targetPos, Vector3 up)
 	:Camera(eyePos, targetPos, up)
 {
-
+	SetLens(0.25f, Application::GetInstance()->GetAspectRatio(), 1.0f, 1000.0f);
 }
