f92a6f36407bf1ec252da597f3881f907eb722c5
jwvg0425 <jwvg0425@naver.com>
2015-05-11 21:34:39 +0900

    [engine] vertex impl

--- src/render/vertex/posColor.h
+++ src/render/vertex/posColor.h
@@ -1,10 +1,12 @@
 ﻿#include "base/macro.h"
+#include "math/vector.h"
 
 NS_EEL_BEGIN
 
 struct PosColor
 {
-
+	Point3 m_Pos;
+	Color4 m_Color;
 };
 
 NS_EEL_END
--- src/render/vertex/posNormal.h
+++ src/render/vertex/posNormal.h
@@ -1,10 +1,12 @@
 ﻿#include "base/macro.h"
+#include "math/vector.h"
 
 NS_EEL_BEGIN
 
 struct PosNormal
 {
-
+	Point3 m_Pos;
+	Vector3 m_Normal;
 };
 
 NS_EEL_END
