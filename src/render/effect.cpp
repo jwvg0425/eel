e8094e7041139c9a8d0ae86de94e25c21906a6e5
ozt88 <kl1467@gmail.com>
2015-05-11 21:58:26 +0900

    [engine] make effect

--- src/render/effect.cpp
+++ src/render/effect.cpp
@@ -1,1 +1,73 @@
-﻿
+﻿#include "effect.h"
+#include <fstream>
+#include "base\renderer.h"
+#include "utility\debug.h"
+
+USING_NS_EEL;
+
+EffectImpl::EffectImpl(const std::wstring& filePath, std::vector<EffectImpl::TypePair>& typePairs)
+{
+	std::ifstream fin(filePath, std::ios::binary);
+
+	fin.seekg(0, std::ios_base::end);
+	int size = (int) fin.tellg();
+	fin.seekg(0, std::ios_base::beg);
+	std::vector<char> compiledShader(size);
+
+	fin.read(&compiledShader[0], size);
+	fin.close();
+
+	HR(D3DX11CreateEffectFromMemory(&compiledShader[0], size, 0, Renderer::GetInstance()->GetDevice(), &m_Fx));
+
+	for(auto pair : typePairs)
+	{
+		GetNewMember(pair);
+	}
+}
+
+void EffectImpl::GetNewMember(const TypePair& pair)
+{
+	switch(pair.second)
+	{
+		case EffectValueType::TECH:
+		{
+			TechPair newPair;
+			newPair.first = pair.first;
+			newPair.second = m_Fx->GetTechniqueByName(pair.first.c_str());
+			m_Techs.push_back(newPair);
+			break;
+		}
+		case EffectValueType::GENERIC:
+		{
+			GenericPair newPair;
+			newPair.first = pair.first;
+			newPair.second = m_Fx->GetVariableByName(pair.first.c_str());
+			m_GenericValues.push_back(newPair);
+		}
+		case EffectValueType::MATRIX:
+		{
+			MatrixPair newPair;
+			newPair.first = pair.first;
+			newPair.second = m_Fx->GetVariableByName(pair.first.c_str())->AsMatrix();
+			m_Matrixes.push_back(newPair);
+		}
+		case EffectValueType::VECTOR:
+		{
+			VectorPair newPair;
+			newPair.first = pair.first;
+			newPair.second = m_Fx->GetVariableByName(pair.first.c_str())->AsVector();
+			m_Vectors.push_back(newPair);
+		}
+		case EffectValueType::RESOURCE:
+		{
+			ResourcePair newPair;
+			newPair.first = pair.first;
+			newPair.second = m_Fx->GetVariableByName(pair.first.c_str())->AsShaderResource();
+			m_Resources.push_back(newPair);
+		}
+		default:
+		{
+			break;
+		}
+	}
+}
--- src/render/effect.h
+++ src/render/effect.h
@@ -1,19 +1,123 @@
 ﻿#pragma once
 #include "base/macro.h"
 #include "utility/singleton.h"
+#include <d3dx11Effect.h>
+#include <vector>
+// const buffer / tech list
+// getVariable, setVariable,
 
 NS_EEL_BEGIN
+
+enum class EffectValueType
+{
+	GENERIC,
+	VECTOR,
+	MATRIX,
+	RESOURCE,
+	TECH,
+};
 
 class EffectImpl
 {
 public:
+	using TechPair = std::pair < std::string, ID3DX11EffectTechnique* > ;
+	using TypePair = std::pair < std::string, EffectValueType > ;
+	using MatrixPair = std::pair < std::string, ID3DX11EffectMatrixVariable* > ;
+	using VectorPair = std::pair < std::string, ID3DX11EffectVectorVariable* > ;
+	using GenericPair = std::pair < std::string, ID3DX11EffectVariable* > ;
+	using ResourcePair = std::pair < std::string, ID3DX11EffectShaderResourceVariable* > ;
+
+public:
+
+	EffectImpl(const std::wstring& fileName, std::vector<EffectImpl::TypePair>& typePairs);
+	~EffectImpl();
+
+	template<typename T>
+	bool	SetMember(const std::wstring& memberName, T value, EffectValueType valueType, UINT valueSize);
+private:
+	void	GetNewMember(const TypePair& pair);
 
 private:
+	ID3DX11Effect* m_Fx = nullptr;
+	std::vector<TechPair> m_Techs;
+	std::vector<MatrixPair> m_Matrixes;
+	std::vector<VectorPair> m_Vectors;
+	std::vector<GenericPair> m_GenericValues;
+	std::vector<ResourcePair> m_Resources;
 };
+
+template<typename T>
+bool eel::EffectImpl::SetMember(const std::wstring& memberName, T value, EffectValueType valueType, UINT valueSize)
+{
+
+	switch(valueType)
+	{
+		case eel::EffectValueType::GENERIC:
+		{
+			for(auto pair : m_GenericValues)
+			{
+				if(pair.first == memberName)
+				{
+					pair.second->SetRawValue(&value, 0, valueSize);
+					return true;
+				}
+			}
+			break;
+		}
+		case eel::EffectValueType::VECTOR:
+		{
+			for(auto pair : m_Vectors)
+			{
+				if(pair.first == memberName)
+				{
+					pair.second->SetRawValue(&value, 0, valueSize);
+					return true;
+				}
+			}
+			break;
+		}
+		case eel::EffectValueType::MATRIX:
+		{
+			static_assert( std::is_same(T, CXMMATRIX)::value );
+			for(auto pair : m_Matrixes)
+			{
+				if(pair.first == memberName)
+				{
+					pair.second->SetMatrix(reinterpret_cast<const float*>( &value ));
+					return true;
+				}
+			}
+			break;
+		}
+		case eel::EffectValueType::RESOURCE:
+		{
+			static_assert( std::is_same(T, ID3D11ShaderResourceView*)::value );
+			for(auto pair : m_Resources)
+			{
+				if(pair.first == memberName)
+				{
+					pair.second->SetResource(value);
+					return true;
+				}
+			}
+			break;
+		}
+		default:
+		break;
+	}
+
+	return false;
+}
 
 template <typename T>
 class Effect : public EffectImpl, public Singleton<T>
 {
+public:
+	Effect(const std::wstring& fxPath, const std::vector<TypePair>& typePairs)
+		:EffectImpl(fxPath, typePairs)
+	{};
+	~Effect();
+private:
 };
 
 NS_EEL_END
