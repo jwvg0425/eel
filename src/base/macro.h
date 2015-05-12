49a0831b90036375af94a7ce03341d9a986ac7cc
jwvg0425 <jwvg0425@naver.com>
2015-05-12 10:07:02 +0900

    [engine] remove type traits alias(it already exists)

--- src/base/macro.h
+++ src/base/macro.h
@@ -104,16 +104,6 @@ using ARRAY2D = std::array<std::array<T, COL>, ROW>;
 template<typename T, int A, int B, int C>
 using ARRAY3D = std::array<std::array<std::array<T, C>, B>, A > ;
 
-//type traits alias
-template<typename T>
-using decay_t = typename std::decay<T>::type;
-
-template<bool test>
-using enable_if_t = typename std::enable_if<test>::type;
-
-template<typename T>
-using remove_reference_t = typename std::remove_reference<T>::type;
-
 //bit macro function
 template<typename T1, typename T2>
 void SetBit(T1& dest, T2 bit)
