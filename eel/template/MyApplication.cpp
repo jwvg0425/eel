6f8b791530f30411f6280727563a75d97ee3f5ed
jwvg0425 <jwvg0425@naver.com>
2015-05-13 09:35:35 +0900

    [template] camera create

--- eel/template/MyApplication.cpp
+++ eel/template/MyApplication.cpp
@@ -7,7 +7,7 @@ void MyApplication::FinishLaunching()
 		SetScreenBackgroundColor(eel::Color4(0.6f, 0.3f, 0.3f, 1.0f));
 
 	eel::Renderer::GetInstance()->
-		SetScreenCamera(new eel::PerspectiveCamera(eel::Point3(0.0f, 20.0f, 30.0f), eel::Point3(0.0f, 0.0f, 0.0f), eel::Vector3(0.0f, 1.0f, 0.0f)));
+		SetScreenCamera(eel::PerspectiveCamera::Create(eel::Point3(0.0f, 20.0f, 30.0f), eel::Point3(0.0f, 0.0f, 0.0f), eel::Vector3(0.0f, 1.0f, 0.0f)));
 
 	eel::Director::GetInstance()->
 		RunWithScene(FirstScene::Create());
