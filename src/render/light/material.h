8b49d172640d49129ec1ee8c06ab7539c52c9f0e
jwvg0425 <jwvg0425@naver.com>
2015-05-20 18:18:55 +0900

    [engine] add material

--- src/render/light/material.h
+++ src/render/light/material.h
@@ -1,14 +1,27 @@
-﻿#include "base/macro.h"
+﻿#pragma once
+#include "base/macro.h"
 #include "math/vector.h"
 
 NS_EEL_BEGIN
 
+
+//just interface
 struct Material
+{
+};
+
+struct LightMaterial : public eel::Material
 {
 	Color4 m_Ambient;
 	Color4 m_Diffuse;
 	Color4 m_Specular;
 	Color4 m_Reflect;
+};
+
+struct MaterialData
+{
+	Material* m_Material;
+	UINT m_Size;
 };
 
 NS_EEL_END
